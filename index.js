954d0b1db68e9fa8a5c08d8bb0b47db79318f62b
Emilio <emilioplatzer@gmail.com>
2015-03-01 02:09:26 -0300

    Arreglo el controlador para que funcione con una aridad hasta 6

--- index.js
+++ index.js
@@ -27,12 +27,23 @@ ec.control = function control(object, functionName){
         originalFunction: theOldFunction,
         stopControl: this.stopControl
     };
-    var theControledFunction = function(){
+    var theControledFunction0 = function(){
         theControl.calls.push({
             This:this===ec.globalObject?ec.global:this, args:Array.prototype.slice.call(arguments)
         });
         return theOldFunction.apply(this,arguments);
     };
+    var theControledFunction;
+    switch(theOldFunction.length){
+        case 0: theControledFunction=theControledFunction0; break;
+        case 1: theControledFunction=function(a){ return theControledFunction0.apply(this,arguments); }; break;
+        case 2: theControledFunction=function(a,b){ return theControledFunction0.apply(this,arguments); }; break;
+        case 3: theControledFunction=function(a,b,c){ return theControledFunction0.apply(this,arguments); }; break;
+        case 4: theControledFunction=function(a,b,c,d){ return theControledFunction0.apply(this,arguments); }; break;
+        case 5: theControledFunction=function(a,b,c,d,e){ return theControledFunction0.apply(this,arguments); }; break;
+        case 6: theControledFunction=function(a,b,c,d,e,f){ return theControledFunction0.apply(this,arguments); }; break;
+        default: throw new Error('not implemented control for function with arity greater than 6');
+    }
     object[functionName]=theControledFunction;
     return theControl;
 };
