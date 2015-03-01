2245a55623997d8fb205ba1ee4d67bb54a6628ca
Emilio <emilioplatzer@gmail.com>
2015-03-01 01:38:59 -0300

    Hace el control como se espera.

--- index.js
+++ index.js
@@ -11,12 +11,34 @@
 var ec={
 };
 
+ec.globalObject = function(){ return this; }();
+
+ec.global = function(){ return this.globalObject; };
+
 ec.control = function control(object, functionName){
-    return {
-        stopControl: function stopControl(object, functionName){
-            return null;
-        }
+    if(!(functionName in object)){
+        throw new Error('expect-called.control: function '+functionName+' does not exists in the object');
+    }
+    var theOldFunction = object[functionName];
+    var theControl = {
+        calls: [],
+        This: object===this.globalObject?this.global:object,
+        functionName: functionName,
+        originalFunction: theOldFunction,
+        stopControl: this.stopControl
     };
-}
+    var theControledFunction = function(){
+        theControl.calls.push({
+            This:this===ec.globalObject?ec.global:this, args:Array.prototype.slice.call(arguments)
+        });
+        return theOldFunction.apply(this,arguments);
+    };
+    object[functionName]=theControledFunction;
+    return theControl;
+};
+
+ec.stopControl = function stopControl(){
+    (this.This===ec.global?ec.globalObject:this.This)[this.functionName]=this.originalFunction;
+};
  
 exports = module.exports = ec;
--- test/test.js
+++ test/test.js
@@ -10,17 +10,28 @@ var globalCount=0
 var theLastThis=null;
 var theLastArgs=null;
 
-function globalFunction(x){
+theGlobal.globalFunction = function(x){
     globalCount++;
     theLastThis=this;
     theLastArgs=Array.prototype.slice.call(arguments);
     return globalCount+100;
 }
 
+var theGlobalFunction=globalFunction;
+var theOriginalGlobalFunction=globalFunction;
+// var theGlobalFunctionContainer=this;
+var theGlobalFunctionContainer=theGlobal;
+
 describe('expect-called', function(){
     describe('basic test', function(){
+        it('should control that function exists',function(){
+            var emptyObject={};
+            expect(function(){
+                expectCalled.control(emptyObject,'fn');
+            }).to.throwError('/function fn does not exists in the object/');
+        });
         it('should call controled function', function(){
-            var control=expectCalled.control(theGlobal,'globalFunction');
+            var control=expectCalled.control(theGlobalFunctionContainer,'globalFunction');
             var x={};
             var y=43;
             var returned=globalFunction(x,y);
@@ -29,12 +40,23 @@ describe('expect-called', function(){
             ]).to.eql([
                 1          ,theGlobal  ,[x,y]      ,101
             ]);
-            expect(control).to.eql({
-                This: theGlobal,
+            var expected={
+                calls: [{This: expectCalled.global, args: [x,y]}],
+                This: expectCalled.global,
                 functionName: 'globalFunction',
-                calls: [{This: theGlobal, args: [x,y]}],
+                originalFunction: theOriginalGlobalFunction,
                 stopControl: expectCalled.stopControl
-            });
+            };
+            /*
+            expect(control.calls).to.eql(expected.calls);
+            expect(control.This).to.eql(expected.This);
+            expect(control.functionName).to.eql(expected.functionName);
+            expect(control.originalFunction).to.eql(expected.originalFunction);
+            expect(control.stopControl).to.eql(expected.stopControl);
+            */
+            expect(control).to.eql(expected);
+            control.stopControl();
+            expect(globalFunction).to.be(theGlobalFunction);
         });
     });
 });
